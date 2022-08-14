b85b0bc1b42b10ebc6f354162910e699d1686209
Aleksandr <budkin86bk@yandex.ru>
2022-08-14 11:39:57 +0500

    Вторая лекция

---
+++ Example010_Find/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число поиска ");
+int find = Convert.ToInt32(Console.ReadLine());
+int[] array = { 12, 4, 6, 8, 2, 12 };
+int n = array.Length;
+int index = 0;
+// for (int index = 0; index < n; index++)
+// {
+//     if (array[index] == find) Console.WriteLine("Число совпало на " + find);
+//     else Console.WriteLine("Число не совпало");
+// }
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    
+    index++;
+}
+
+
---
+++ Example011/Program.cs
@@ -0,0 +1,53 @@
+﻿void FillArray(int[] collection)
+
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int positionFind = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            positionFind = index;
+            break;
+        }
+        index++;
+    }
+    return positionFind;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+Console.Write("Введите элемент для поиска: ");
+int find = Convert.ToInt32(Console.ReadLine());
+int pos = IndexOf(array, find);
+Console.Write("Искомый элемент: " + pos);
+
+
+
